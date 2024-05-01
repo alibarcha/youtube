1d5030eab12da98a7f8e020eeecdc8c801847af1
Ali Haider <alibarchadev@gmail.com>
2024-05-01 16:44:42 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  Home Page
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
